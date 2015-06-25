b73bdcf8890f089f296a04c2eda16addbae58f50
hugohil <hugo@soixantecircuits.fr>
2015-06-25 16:55:55 +0200

    swagify this

--- client/js/globals/toast.js
+++ client/js/globals/toast.js
@@ -1,11 +1,14 @@
 Template.toaster.helpers({
   'toasts': function () {
     // TODO: store timeout to clear it
+    // TODO: check why this event is triggered twice
     setTimeout(function() {
+      $('.toast-container').fadeOut();
       console.log('reset toasts');
       Session.set('toasts', []);
       return Session.get('toasts');
     }, 5000);
+    $('.toast-container').fadeIn();
     return Session.get('toasts') || [];
   }
 });
