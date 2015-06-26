704a5d1349764a6d5fc229d6a426095e5f7c0b00
hugohil <hugo@soixantecircuits.fr>
2015-06-26 14:38:43 +0200

    Internationalisation

--- client/js/globals/i18n.js
+++
@@ -1,5 +0,0 @@
-// Translation
-i18n.init({ lng: 'fr-FR', load: 'current', fallbackLng: false }, function(t) {
-  // translate nav
-  $('body').i18n();
-});
---
+++ client/js/globals/i18next.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  i18n.init({ lng: 'en-EN', load: 'current', fallbackLng: false }, function(t) {
+    $('[data-i18n]').i18n();
+  });
+});
