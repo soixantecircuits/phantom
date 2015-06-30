f95a34519cf17072403eadb7367fcdb42b43d0a7
hugohil <hugo@soixantecircuits.fr>
2015-06-30 13:09:31 +0200

    Lame hack for validation email redirection

--- router/router.js
+++ router/router.js
@@ -1,7 +1,26 @@
 'use strict';
 
 Router.route('/', function(){
-  this.render('login');
+  if (Accounts._verifyEmailToken) {
+    var self = this;
+    this.next();
+    Accounts.verifyEmail(Accounts._verifyEmailToken, function(err) {
+      if (err != null) {
+        if (err.message = 'Verify email link expired [403]') {
+          Session.set('toasts', [{content: 'Sorry this verification link has expired.'}]);
+          // lame ...
+          window.location = '/';
+        } else {
+          window.location = '/';
+        }
+      } else {
+        Session.set('toasts', [{content: 'Thank you! Your email address has been confirmed.'}]);
+        self.redirect('/dashboard');
+      }
+    });
+  } else {
+    this.render('login');
+  }
 });
 
 Router.route('/signup');
--- server/enrolment.js
+++ server/enrolment.js
@@ -3,3 +3,12 @@ Meteor.methods({
     Accounts.sendVerificationEmail(Meteor.userId(), email);
   }
 });
+
+Accounts.validateLoginAttempt(function(attempt){
+  if (attempt.user && attempt.user.emails && !attempt.user.emails[0].verified ) {
+    console.log('email not verified');
+
+    return false; // the login is aborted
+  }
+  return true;
+});
