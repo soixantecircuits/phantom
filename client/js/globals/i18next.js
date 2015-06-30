5910eba21930188bffeb1e5e2d4435c4c40c5614
hugohil <hugo@soixantecircuits.fr>
2015-06-30 10:28:03 +0200

    Translate content on each route change

--- client/js/globals/i18next.js
+++ client/js/globals/i18next.js
@@ -1,5 +1,6 @@
-$(document).ready(function(){
+Router.onBeforeAction(function() {
   i18n.init({ lng: 'en-EN', load: 'current', fallbackLng: false }, function(t) {
     $('[data-i18n]').i18n();
   });
+  this.next();
 });
