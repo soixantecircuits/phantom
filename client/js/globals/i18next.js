e386ac6206ad0a87801f8adf6490e2419a0dab82
hugohil <hugo@soixantecircuits.fr>
2015-06-30 12:12:56 +0200

    better fix on translation

--- client/js/globals/i18next.js
+++ client/js/globals/i18next.js
@@ -1,6 +1,8 @@
 Router.onBeforeAction(function() {
-  i18n.init({ lng: 'en-EN', load: 'current', fallbackLng: false }, function(t) {
-    $('[data-i18n]').i18n();
+  $(document).ready(function(){
+    i18n.init({ lng: 'en-EN', load: 'current', fallbackLng: false }, function(t) {
+      $('[data-i18n]').i18n();
+    });
   });
   this.next();
 });
